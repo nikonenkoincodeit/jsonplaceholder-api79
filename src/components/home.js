2d63129f9e3c37aef3cee8fafa1c13f8280420b3
OleksandKr <cfif357@ukr.net>
2023-05-13 16:48:30 +0300

    add function onclick

--- src/components/home.js
+++ src/components/home.js
@@ -8,12 +8,20 @@ window.addEventListener("load", init);
 function init() {
   getData("users")
     .then((data) => {
-      console.log(data);
-      const markup = makeFirstTableMarkup(data);
-      console.log(markup);
+      const markup = makeFirstTableMarkup(data)
       addMarkup(markup, tablBodyHomeEl);
     })
     .catch((error) => {
       console.log(error.message);
     });
 }
+
+tablBodyHomeEl.addEventListener('click', onClick);
+
+function onClick(e) {
+  const trEL = e.target.closest('tr');
+  if (trEL.nodeName !== 'TR') return
+  const userId = trEL.dataset.userid;
+  location.href = `user.html?userid=${userId}`;
+  console.log(userId);
+};
