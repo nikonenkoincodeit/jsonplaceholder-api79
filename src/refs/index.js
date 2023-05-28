ed0747c0b58fa8920760aec77bf861fa158b877a
nikonenkoincodeit <75611895+nikonenkoincodeit@users.noreply.github.com>
2023-05-28 10:09:45 +0300

    start project

--- src/refs/index.js
+++
@@ -1,5 +0,0 @@
-const tablBodyHomeEl = document.querySelector('.js-tbody');
-const tablBodyUserEl = document.querySelector('.js-user-table');
-const userAlbumListEl = document.querySelector('.js-album');
-
-export { tablBodyHomeEl, tablBodyUserEl, userAlbumListEl };
